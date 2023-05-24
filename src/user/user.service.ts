c4d6ada7ba57c5ab021f0ff81e78a9fb8e1c879d
Thiago Holanda <thimholanda@gmail.com>
2023-05-23 22:30:27 -0300

    feat(user) remove async form `findByEmail`

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -21,7 +21,7 @@ export class UserService {
     return { ...createdUser, password: undefined };
   }
 
-  async findByEmail(email: string) {
+  findByEmail(email: string) {
     return this.prisma.user.findUnique({
       where: { email },
     });
