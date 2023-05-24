2bb301c0e93bc74d3232c005fef29c02a6064593
Thiago Holanda <thimholanda@gmail.com>
2023-05-24 00:39:50 -0300

    feat(auth): implement `@CurrentUser()`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('user')
 export class UserController {
